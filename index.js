4777bb63e285207d6cb496ad8d9f29d759345953
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2023-04-30 21:07:37 +0300

    remove unused imports

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const nodemailer = require("nodemailer");
-const sendOTP = require("./sendOTP");
 let cors = require("cors");
 const app = express();
 const port = 3000;
